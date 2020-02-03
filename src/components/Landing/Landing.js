14c334043829875140b761122922e38054604809
albertob <albertob@qu-dvmcws-4808.corp.ctv.ca>
2020-02-03 11:22:24 -0500

    BEM naming for Landing

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,12 +4,11 @@ import './Landing.scss';
 
 const Landing = (props) => {
     return(
-        <div id="Landing" className="column">
-            <div className="copyWrapper">
-                <h1>albertobonora.com</h1>
-                <h2>{props.year + " " + props.title}</h2>
+        <div className="landing">
+            <div className="landing__copyWrapper">
+                <h1 className="landing__siteTitle">albertobonora.com</h1>
+                <h2 className="landing__appTitle">{props.year + " " + props.title}</h2>
             </div>
-            {/* <img src={bg} alt="reading challenge"/> */}
         </div>
     )
 }
