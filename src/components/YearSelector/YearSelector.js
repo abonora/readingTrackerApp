c4c3aca1f2df428de375c50258672d46948bcec5
albertob <albertob@qu-dvmcws-4808.corp.ctv.ca>
2020-02-03 11:13:23 -0500

    BEM naming for year selector

--- src/components/YearSelector/YearSelector.js
+++ src/components/YearSelector/YearSelector.js
@@ -4,12 +4,12 @@ import './YearSelector.scss';
 const yearSelector = (props) => {
     //console.log(props.years[1])
     return(
-        <div className="yearWrapper">
-            <h3>Years:</h3>
-            <ul>
+        <div className="year">
+            <h3 className="year__label">Years:</h3>
+            <ul className="year__list">
                 {/* <li class="selected">2019</li> */}
                 {props.years.map(function(item){
-                    return <li className={item.year === props.currentYear ? 'selected' : ''} onClick={props.changeEvent} key={item.year} data-year={item.year} data-shelfid={item.bookShelf}>{item.year}</li>
+                    return <li className={item.year === props.currentYear ? 'year__item year__item--selected' : 'year__item'} onClick={props.changeEvent} key={item.year} data-year={item.year} data-shelfid={item.bookShelf}>{item.year}</li>
                 })}
             </ul>
         </div>
